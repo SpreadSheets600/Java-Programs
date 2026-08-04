5ea1974e0d6f93492ccd8739779c12ad5a119a8a
SpreadSheets600 <sohammaity239@gmail.com>
2026-08-04 19:50:48 +0530

    feat: 04-08-2026

---
+++ 04-08-2026/FactorialSum.java
@@ -0,0 +1,13 @@
+public class FactorialSum {
+    public static void main(String args[]) {
+        int n = 5;
+        int i, fact = 1, sum = 0;
+
+        for (i = 1; i <= n; i++) {
+            fact *= i;
+            sum += fact;
+        }
+
+        System.out.println("The Sum Of Factorials From 1 To " + n + " Is: " + sum);
+    }
+}
